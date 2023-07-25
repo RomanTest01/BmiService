a95180ec347a8a3ffb603fdf1b3b95c39bf5e327
Roman <borisenko0893@gmail.com>
2023-07-25 21:12:54 +0300

    third commit

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
-    public int calculate(double a, double b) {
-        double c = a / (b * b);
-        return (int) c;
+    public int calculate(double weight, double height) {
+        double index = weight / (height * height);
+        return (int) index;
     }
 }
